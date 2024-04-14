688fac9cb8eb2a7c2ee6c6082ca7feedc34fe8ef
EmuTheGreat <thegreatfrut@mail.ru>
2024-04-14 18:13:16 +0500

    Onion-архитектура

--- ProfileApi/ProfileApi/Logic/User/IUserManager.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProfileApi.Logic.User
-{
-    public interface IUserManager
-    {
-        Task<string> GetUserNameAsync(Guid userId);
-        Task<Guid> CreateUserAsync(UserLogic user);
-    }
-}
